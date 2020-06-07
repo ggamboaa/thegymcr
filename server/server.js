5ead42beeba75c2897d94a5288b7769107ee272a
Gilbert Gamboa <gilbert.g.alvarado@gmail.com>
2020-06-07 12:05:34 -0600

    C10
    c10

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const db = require("./server/models/db");
+const db = require("./models/db");
 // var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const seed = require("./server/models/seed/seed-bd");
+const seed = require("./models/seed/seed-bd");
 
 // setup the Express middleware
 //require('./server/middleware/middleware')(app);
 
-app.use(cors());
+// app.use(cors());
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // setup the API
-require("./server/routes/routes")(app);
+require("./routes/routes")(app);
 
 // connect to DB then run server
 db.sequelize
