ef3e786a351ecdf4e7f3aa70571fc83aaa73178b
Gilbert Gamboa <gilbert.g.alvarado@gmail.com>
2020-06-10 13:56:43 -0600

    server.js
    server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const db = require("./models/db");
+const db = require("./server/models/db");
+// var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const seed = require("./models/seed/seed-bd");
+const seed = require("./server/models/seed/seed-bd");
+
+// setup the Express middleware
+//require('./server/middleware/middleware')(app);
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// setup the API
+require("./routes/routes")(app);
+require("./server/routes/routes")(app);
+
+// connect to DB then run server
+db.sequelize
+  .sync({
+    force: true,
+  })
+  // .then(() => {
+  //   seed.insertingMaintenanceData();
+  // })
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Running server on port " + port);
+    });
+  });
